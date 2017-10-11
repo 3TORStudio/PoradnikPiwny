ce11fcd895f9a5a4ab5db4a425a8ad383e165100
Marcin <marcinhesse@gmail.com>
2017-10-11 22:23:53 +0200

    Testowanie tworzenia referencji do obiektów graficznych (TextView).

--- app/src/main/java/com/example/marcin/doradcapiwny/FindBeerActivity.java
+++ app/src/main/java/com/example/marcin/doradcapiwny/FindBeerActivity.java
@@ -2,12 +2,28 @@ package com.example.marcin.doradcapiwny;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
+    private boolean czyKlikniete = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view){
+        //utworzenie zmiennej referencyjnej do pola tekstowego w którym będzie
+        //wyświetlona propozycja piwa
+        TextView tekstZListaPiwa = (TextView) findViewById(R.id.brand);
+        if (!czyKlikniete){
+            tekstZListaPiwa.setText("Na razie nie wiem co zaproponować");
+            czyKlikniete = true;
+        } else {
+            tekstZListaPiwa.setText("Daj spokój. Nic nie powiem");
+        }
+
     }
 }
