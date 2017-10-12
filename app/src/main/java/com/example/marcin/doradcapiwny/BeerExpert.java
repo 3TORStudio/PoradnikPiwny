6d13cd2daff87e685ca6f83ab0010f3054cef074
Marcin <marcinhesse@gmail.com>
2017-10-12 18:07:36 +0200

    nowa klasa BeerExpert

---
+++ app/src/main/java/com/example/marcin/doradcapiwny/BeerExpert.java
@@ -0,0 +1,22 @@
+package com.example.marcin.doradcapiwny;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Marcin on 12.10.2017.
+ */
+
+public class BeerExpert {
+    List<String> getBrands (String color){
+        List<String> brands = new ArrayList<>();
+        if(color == "bursztynowe"){
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+    }
+    return brands;
+}
